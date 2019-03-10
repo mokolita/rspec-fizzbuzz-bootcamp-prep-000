0dbf19a29167272fc6270984ed2022f4f26dba4b
Meg Galvez <meg@oakmaine.com>
2019-03-10 18:00:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(number)
+
+  if number == 3
+    puts "Fizz"
+  elsif number == 5
+    puts "Buzz"
+  elsif number == 15
+    puts "Fizzbuzz"
+  end 
+
+end
